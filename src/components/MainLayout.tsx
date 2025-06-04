2918bd3204c3d6262218d3bd6af3c9ba803e07b6
huutai09072003 <tain6507@gmail.com>
2025-06-04 22:16:44 +0700

    update layout

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import Footer from './Footer';
 import Header from './Header';
 
 const MainLayout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
